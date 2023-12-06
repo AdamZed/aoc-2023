9540d4c03eb84428918549228ad16877055e6848
AdamZed <adamzanon@hotmail.com>
2023-12-05 23:39:41 -0500

    day 1 part 2

--- day01/day1.py
+++ day01/day1.py
@@ -1,5 +1,3 @@
-import re 
-
 class Solution:
 
     def __init__(self, fname):
@@ -17,15 +15,18 @@ class Solution:
             return [x for x in f.read().strip().split()]
             
     def solve(self):
-        total = 0
         for line in self.data:
-            first = None
-            for ch in line:
+            nums1 = []
+            nums2 = []
+            for i, ch in enumerate(line):
                 if ch.isdigit():
-                    last = int(ch)
-                    if first is None: first = last
-            total += first * 10 + last
-        self.ans1 = total
+                    nums1.append(int(ch))
+                    nums2.append(int(ch))
+                for n, num in enumerate(["zero","one","two","three","four","five","six","seven","eight","nine"]):
+                    if line[i:].startswith(num):
+                        nums2.append(n)
+            self.ans1 += nums1[0] * 10 + nums1[-1]
+            self.ans2 += nums2[0] * 10 + nums2[-1]
 
 if __name__ == "__main__":
     import sys
